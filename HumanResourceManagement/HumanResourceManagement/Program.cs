8af3deab408b1956f99cd5750f8f2d720cd8287d
blusik99 <blusik999@outlook.com>
2020-03-13 11:57:13 +0100

    Add connection MySQL

--- HumanResourceManagement/HumanResourceManagement/Program.cs
+++ HumanResourceManagement/HumanResourceManagement/Program.cs
@@ -4,13 +4,38 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
 using System.Text.RegularExpressions;
+using MySql.Data.MySqlClient;
 
 namespace HumanResourceManagement
 {
     class Program
     {
         static void Main(string[] args)
+
+
         {
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string myConnectionString;
+
+            myConnectionString = "Server=127.0.0.1;Port=3308;Database=mydb;Uid=root;Pwd=password";
+
+
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+                Console.WriteLine("Successfull");
+                conn.Close();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Console.WriteLine("Cannot open");
+                
+
+            }
+
+
             HumanResources Staff = new HumanResources();
             Staff.StartMenu();
         }
